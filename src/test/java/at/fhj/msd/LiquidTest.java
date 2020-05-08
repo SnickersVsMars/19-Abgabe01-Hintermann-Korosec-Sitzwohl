0bd2b1f0552f5489f69196c89db1745ee94f5e2b
Marian Korosec <marian.korosec@edu.fh-joanneum.at>
2020-05-08 23:07:42 +0200

    Added messages to tests

--- src/test/java/at/fhj/msd/LiquidTest.java
+++ src/test/java/at/fhj/msd/LiquidTest.java
@@ -18,44 +18,53 @@ public class LiquidTest {
 	@BeforeEach
 	void setup() {
 		// SETUP PHASE
-		lN = new Liquid("Wasser", 0.3, 0);
-		lA = new Liquid("Wein", 0.125, 13);
+		lN = new Liquid("Water", 0.3, 0);
+		lA = new Liquid("Wine", 0.125, 13);
 	}
 
 	@Test
 	@DisplayName("Testing constructor non alcoholic")
 	public void testConstructorNonAlcoholic(){
-		assertEquals("Wasser", lN.getName());
-		assertEquals(0.3, lN.getVolume(), 0.001);
-		assertEquals(0, lN.getAlcoholPercent(), 0.001);
+		assertEquals("Water", lN.getName(),
+				"Expected the name to be \"Water\", but was" + lN.getName());
+		assertEquals(0.3, lN.getVolume(), 0.001,
+				"Expected a volume of .3l, but was" + lN.getVolume());
+		assertEquals(0, lN.getAlcoholPercent(), 0.001,
+				"Expected an alcohol percentage of 0, but was" + lN.getAlcoholPercent());
 	}
 
 	@Test
 	@DisplayName("Testing constructor alcoholic")
 	public void testConstructorAlcoholic(){
-		assertEquals("Wein", lA.getName());
-		assertEquals(0.125, lA.getVolume(), 0.001);
-		assertEquals(13, lA.getAlcoholPercent(), 0.001);
+		assertEquals("Wine", lA.getName(),
+				"Expected the name to be \"Wine\", but was" + lA.getName());
+		assertEquals(0.125, lA.getVolume(), 0.001,
+				"Expected a volume of .125l, but was" + lA.getVolume());
+		assertEquals(13, lA.getAlcoholPercent(), 0.001,
+				"Expected an alcohol percentage of 13, but was" + lA.getAlcoholPercent());
 	}
 
 	@Test
 	@DisplayName("Test name setter")
 	public void testNameSetter(){
 		lN.setName("Granderwasser");
-		assertEquals("Granderwasser", lN.getName());
+		assertEquals("Granderwasser", lN.getName(),
+				"Expected the name to be \"Granderwasser\", but was" + lN.getName());
 	}
 
 	@Test
 	@DisplayName("Testing volume setter")
 	public void testVolumeSetter(){
 		lN.setVolume(0.5);
-		assertEquals(0.5, lN.getVolume(), 0.001);
+		assertEquals(0.5, lN.getVolume(), 0.001,
+				"Expected a volume of .5l, but was" + lN.getVolume());
 	}
 
 	@Test
 	@DisplayName("Testing alcoholPercent setters")
 	public void testAlcoholPercentSetter(){
 		lN.setAlcoholPercent(3.2);
-		assertEquals(3.2, lN.getAlcoholPercent(), 0.001);
+		assertEquals(3.2, lN.getAlcoholPercent(), 0.001,
+				"Expected an alcohol percentage of 3.2, but was" + lN.getAlcoholPercent());
 	}
 }
